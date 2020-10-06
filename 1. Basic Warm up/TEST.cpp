cb10fd8e5d4cc57deee194e13b51a892149a889b
Sanket Patil <61304918+sanketpatil02@users.noreply.github.com>
2020-10-06 15:19:01 +0530

    Life, the Universe, and Everything

---
+++ 1. Basic Warm up/TEST.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> 
+
+#define magic   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll  long long int
+#define v   vector<ll>
+#define it  iterator it
+#define pb  push_back
+#define f(a, b)     for(ll i=a; i<b; i++)
+
+int max(int a, int b) {return (a>b) ? a:b;}
+int min(int a, int b) {return (a<b) ? a:b;}
+
+using namespace std;
+
+int main() {
+    
+	while(1)
+	{
+	    int n;
+	    cin >> n;
+	    if(n==42)
+	        break;
+	        
+	    cout << n << endl;
+	}
+	return 0;
+}
